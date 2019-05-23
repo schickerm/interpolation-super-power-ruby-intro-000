84b8f93802fdd185cab63ec7e749c8b176f4622b
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-23 18:59:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,4 @@
 def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
   puts "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]}"
 end
-display_rainbow(color)
+display_rainbow
